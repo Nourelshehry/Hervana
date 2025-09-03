5a2712d134212e1bfebd414ac2792250955169f8
Nour <nourthranduil@gmail.com>
2025-09-03 21:59:42 +0300

    fixed

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ document.addEventListener("DOMContentLoaded", function () {
     alert("Welcome to Hervana! Let's start shopping 🛍️");
   });
 });
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav");
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
